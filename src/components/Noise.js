6c13391e313a50007a81278eccf440840091c95b
sushant <ksushant6566@gmail.com>
2020-09-05 21:55:28 +0530

    like dislike feature added

--- src/components/Noise.js
+++ src/components/Noise.js
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import { MdDelete } from 'react-icons/md';
+import { MdDelete , MdThumbUp, MdThumbDown} from 'react-icons/md';
 import { LizardContext } from '../context/context';
 
 function Noise(props) {
 
-    const { deleteNoise } = useContext(LizardContext);
+    const { deleteNoise, likeNoise, dislikeNoise } = useContext(LizardContext);
 
     useEffect(() =>console.log(props.noise),[]);
 
@@ -15,7 +15,12 @@ function Noise(props) {
             <br/>
             <h3>{props.noise.author}</h3>
             <p>{props.noise.likes}</p>
-            <MdDelete size="30px" onClick={() => deleteNoise(props.noise._id, props.noise.authorId)} />
+            
+            <div onClick={ () => likeNoise(props.noise._id)}>
+                <MdThumbUp size="25px" />
+                {/* <MdThumbDown size="25px" /> */}
+            </div>
+            <MdDelete size="25px" onClick={() => deleteNoise(props.noise._id, props.noise.authorId)} />
         </div>
 
             <Link to= {`/user/${props.noise.authorId}`} className="user-link">
